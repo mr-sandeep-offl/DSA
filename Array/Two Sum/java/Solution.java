d1ceeb0966f7a5bfedf342a0c1ca23aa046f6d44
Sandeep V <ssandeep79154@gmail.com>
2026-07-20 21:16:47 +0530

    feat: Add Two Sum

--- Array/Two Sum/java/Solution.java
+++ Array/Two Sum/java/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length-1;i++){
-            for (int j=1;j<n;j++){
+            for (int j=1;j<nums.length;j++){
                 int sum=nums[i]+nums[j];
 
                 if(sum==target){
@@ -10,5 +10,6 @@ class Solution {
                 
             }
         }
+        return new int[]{-1,-1};
     }
 }
